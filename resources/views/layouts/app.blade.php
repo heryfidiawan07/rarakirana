9197cc55bca22a21ffbdabf8a2c0402fb45e0946
one07 <heryfidiawan07@gmail.com>
2018-06-10 11:58:40 +0700

    mod social login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,8 +40,8 @@
             'csrfToken' => csrf_token(),
         ]); ?>
     </script>
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120528530-1"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
@@ -54,27 +54,6 @@
     <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
-    <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '{251115105631051}',
-          cookie     : true,
-          xfbml      : true,
-          version    : '{latest-api-version}'
-        });
-          
-        FB.AppEvents.logPageView();   
-          
-      };
-
-      (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "https://connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-    </script>
     
     <div id="app">
         @include('layouts.navigation')
